1da6dcec959b5136919bb81c2c67fb997d752cc7
softparkit <softparkit45@gmail.com>
2024-10-19 22:52:36 +0600

    form submit data show done

--- src/App.vue
+++ src/App.vue
@@ -31,12 +31,12 @@
         <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3">
           <div class="mb-3">
             <div class="form-check">
-              <input class="form-check-input" type="checkbox" id="sendmail" value="SendMail">
+              <input class="form-check-input" type="checkbox" id="sendmail" value="SendMail" v-model="sendMail">
               <label class="form-check-label" for="sendmail">Send Mail</label>
             </div>
             <div class="form-check">
-              <input class="form-check-input" type="checkbox" id="sendInfomail" value="SendInfoMail">
-              <label class="form-check-label" for="sendInfomail">Send Infomail</label>
+              <input class="form-check-input" type="checkbox" id="sendInfomail" value="SendInfoMail" v-model="sendMail">
+              <label class="form-check-label" for="sendInfomail">Send Informal</label>
             </div>
           </div>
         </div>
@@ -45,11 +45,11 @@
         <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3">
           <div class="mb-3">
             <div class="form-check">
-              <input class="form-check-input" type="radio" name="gender" id="male" value="Male">
+              <input class="form-check-input" type="radio" name="gender" id="male" value="Male" v-model="gender">
               <label class="form-check-label" for="male">Male</label>
             </div>
             <div class="form-check">
-              <input class="form-check-input" type="radio" name="gender" id="female" value="Female">
+              <input class="form-check-input" type="radio" name="gender" id="female" value="Female" v-model="gender">
               <label class="form-check-label" for="female">Female</label>
             </div>
           </div>
@@ -59,8 +59,8 @@
         <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3">
           <div class="mb-3">
             <label for="priority" class="form-label">Priority</label>
-            <select id="priority" class="form-select">
-              <option></option>
+            <select id="priority" class="form-select" v-model="selectPrioritey">
+              <option v-for="priority in priorities" :key="priority">{{priority}}</option>
             </select>
           </div>
         </div>
@@ -68,12 +68,12 @@
       <hr>
       <div class="row">
         <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3">
-          <button type="submit" class="btn btn-primary">Submit!</button>
+          <button type="submit" class="btn btn-primary" @click.prevent="submited">Submit!</button>
         </div>
       </div>
     </form>
     <hr>
-    <div class="row">
+    <div class="row" v-if="isSubmited">
       <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3">
         <div class="card">
           <div class="card-header">
@@ -86,10 +86,12 @@
             <p style="white-space: pre-line">Message:{{message}}</p>
             <p><strong>Send Mail?</strong></p>
             <ul>
-              <li></li>
+              <li v-for="mail in sendMail" :key="mail">
+                {{mail}}
+              </li>
             </ul>
-            <p>Gender:</p>
-            <p>Priority:</p>
+            <p>Gender: {{ gender }}</p>
+            <p>Priority: {{selectPrioritey}}</p>
             <p>Switched:</p>
           </div>
         </div>
@@ -107,7 +109,17 @@ export default {
         password: '',
         age: 0,
       },
-      message: ''
+      message: '',
+      sendMail: [],
+      gender: 'Male',
+      selectPrioritey: 'Medium',
+      priorities: ['High', 'Medium', 'Low'],
+      isSubmited: false
+    }
+  },
+  methods:{
+    submited(){
+      this.isSubmited = true;
     }
   }
 }
