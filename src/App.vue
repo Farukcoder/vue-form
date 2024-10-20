536b9415ecffe00671e0b14809691c919e603f24
softparkit <softparkit45@gmail.com>
2024-10-20 07:01:42 +0600

    local custome directory create done

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,24 @@
     <div class="container">
       <div v-awesome.red.big="'faruk'"></div>
       <div v-awesome.green.big="'faruk'"></div>
+      <hr>
+      <div v-user="uservalue"></div>
     </div>
 </template>
 
 <script>
 export default {
-
+  data(){
+    return {
+      uservalue: 'franky'
+    }
+  },
+  directives: {
+    'user': {
+      bind(el, binding) {
+        el.innerHTML = binding.value
+      }
+    }
+  }
 }
 </script>
