752dce7a39d18cbb537800e763f385bddc60f147
softparkit <softparkit45@gmail.com>
2024-10-20 06:49:03 +0600

    globally custome directive create done

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,12 @@
 <template>
-  <div class="container">
-    <button @click="componentToRender='HomePage'">Home</button>
-    <button @click="componentToRender='ContactPage'">Contact</button>
-    <button @click="componentToRender='AboutPage'">About</button>
-    <keep-alive>
-      <component :is="componentToRender"></component>
-    </keep-alive>
-  </div>
+    <div class="container">
+      <div v-awesome.red.big="'faruk'"></div>
+      <div v-awesome.green.big="'faruk'"></div>
+    </div>
 </template>
 
 <script>
-import HomePage from "./components/HomePage.vue";
-import AboutPage from "./components/AboutPage.vue";
-import ContactPage from "./components/ContactPage.vue";
-
 export default {
-  data() {
-    return {
-      componentToRender: "HomePage"  // default component
-    };
-  },
-  components: {
-    HomePage,
-    AboutPage,
-    ContactPage
-  }
-};
+
+}
 </script>
--- src/main.js
+++ src/main.js
@@ -5,6 +5,33 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 Vue.config.productionTip = false
 
+Vue.directive('awesome', {
+  bind(el,binding) {
+    el.innerHTML = binding.value
+    // el.style.color = 'red'
+    // if (binding.arg == 'green'){
+    //   el.style.color = 'green'
+    // }
+    // if (binding.arg == 'red'){
+    //   el.style.color = 'red'
+    // }
+
+    if (binding.modifiers.green){
+      el.style.color = 'green'
+    }
+    if (binding.modifiers.red){
+      el.style.color = 'red'
+    }
+    if (binding.modifiers.big){
+      el.style.fontSize = '30px'
+    }
+    if (binding.modifiers.small){
+      el.style.fontSize = '13px'
+    }
+  }
+})
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
